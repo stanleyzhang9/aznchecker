52a7db2af6a71da3ed35300e7295374820051f84
Stanley Zhang <stanley@stanleys-mbp.dynamic.ucsd.edu>
2018-01-13 02:13:13 -0800

    adding dependencies

--- echoserver.py
+++ echoserver.py
@@ -39,7 +39,7 @@ def handle_messages():
       fh.close()
       print(sender)
       send_message(PAT, sender, 'I have added the item ' + x[0] + ' for the maximum price of ' + x[1])
-      if sender not in list:
+      if sender not in visited:
         list.append(sender)
         threading.Thread(target=start, args=(sender,)).start()     
   return "sent"
